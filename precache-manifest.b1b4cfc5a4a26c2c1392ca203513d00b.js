c668b279267af0276a19515623903dc0a7671f2c
Nishat Aziz <nishataziz09@gmail.com>
2025-04-18 02:02:28 -0400

    Updates

---
+++ precache-manifest.b1b4cfc5a4a26c2c1392ca203513d00b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b4421f9d6a82eaf3c3e3be4ac2cb89",
+    "url": "/Emoji-Dictionary-with-React/index.html"
+  },
+  {
+    "revision": "cd54a0bb4eddbc2953d9",
+    "url": "/Emoji-Dictionary-with-React/static/css/main.5439f17f.chunk.css"
+  },
+  {
+    "revision": "c5684fd1c36404280c27",
+    "url": "/Emoji-Dictionary-with-React/static/js/2.2c75b544.chunk.js"
+  },
+  {
+    "revision": "cd54a0bb4eddbc2953d9",
+    "url": "/Emoji-Dictionary-with-React/static/js/main.78d81edd.chunk.js"
+  },
+  {
+    "revision": "86dec35ae31d8fc53d24",
+    "url": "/Emoji-Dictionary-with-React/static/js/runtime-main.42ee4b37.js"
+  }
+]);
